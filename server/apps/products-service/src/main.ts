9401e2d353c78e0ca13f21fd778a6f04a60a7cb8
amoljagtap17 <amoljagtap17@gmail.com>
2023-09-02 18:05:26 +0530

    fullstack-auth-demo: remove boilerplate code from products-service

--- server/apps/products-service/src/main.ts
+++ server/apps/products-service/src/main.ts
@@ -1,8 +1,21 @@
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { ProductsServiceModule } from './products-service.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(ProductsServiceModule);
-  await app.listen(3000);
+
+  const configService = app.get(ConfigService);
+
+  const PORT = configService.get('PORT');
+  const ORIGINS = configService.get('ORIGINS');
+
+  app.enableCors({ origin: ORIGINS, credentials: true });
+  app.setGlobalPrefix('api');
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  await app.listen(PORT);
 }
 bootstrap();
--- server/apps/products-service/src/products-service.module.ts
+++ server/apps/products-service/src/products-service.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { ProductsServiceController } from './products-service.controller';
-import { ProductsServiceService } from './products-service.service';
 import { ProductsModule } from './products/products.module';
 
 @Module({
   imports: [ProductsModule],
-  controllers: [ProductsServiceController],
-  providers: [ProductsServiceService],
+  controllers: [],
+  providers: [],
 })
 export class ProductsServiceModule {}
