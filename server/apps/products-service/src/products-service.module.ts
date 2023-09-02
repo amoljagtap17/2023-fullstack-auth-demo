c980b116bc128583c0a7bdca7fc5de39dc746fc2
amoljagtap17 <amoljagtap17@gmail.com>
2023-09-02 19:14:25 +0530

    fullstack-auth-demo: add jwt setup to products-service

--- server/apps/products-service/src/products-service.module.ts
+++ server/apps/products-service/src/products-service.module.ts
@@ -1,10 +1,29 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
 import { ProductsModule } from './products/products.module';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), ProductsModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    PassportModule,
+    JwtModule.register({
+      secret: 'abc123',
+      signOptions: { expiresIn: '60s' },
+    }),
+    ProductsModule,
+  ],
   controllers: [],
-  providers: [],
+  providers: [
+    JwtStrategy,
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+  ],
 })
 export class ProductsServiceModule {}
